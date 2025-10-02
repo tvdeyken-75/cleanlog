b68ff888c0480a19b4aa74e76939dfa7804dc155
Timothy Van der Eyken <unified.vandereyken@gmail.com>
2025-10-02 08:01:13 +0000

    I meant the main page of disponent make it an empty one so we can start

--- src/components/disponent/DisponentDashboard.tsx
+++ src/components/disponent/DisponentDashboard.tsx
@@ -1,19 +1,9 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-
 export function DisponentDashboard() {
   return (
-    <div className="p-4 md:p-6">
-      <h1 className="text-3xl font-bold font-headline mb-6">Disponent Dashboard</h1>
-      <Card>
-        <CardHeader>
-          <CardTitle>Starten wir!</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>Dies ist Ihr neues Disponent-Dashboard. Was möchten Sie als Nächstes tun?</p>
-        </CardContent>
-      </Card>
+    <div>
+      {/* Page content will be added here */}
     </div>
   );
 }
