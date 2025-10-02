cbb6de03053373199c2513087989aa2679d0c912
Timothy Van der Eyken <unified.vandereyken@gmail.com>
2025-10-02 08:09:32 +0000

    now I want you to add on to the left top corner disponent page a button

--- src/components/disponent/DisponentDashboard.tsx
+++ src/components/disponent/DisponentDashboard.tsx
@@ -1,9 +1,19 @@
+
 "use client";
+
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 export function DisponentDashboard() {
   return (
-    <div>
-      {/* Page content will be added here */}
+    <div className="p-4">
+      <Button
+        variant="ghost"
+        className="relative h-9 w-9 rounded-full"
+      >
+        <Menu className="h-5 w-5" />
+        <span className="sr-only">Menü öffnen</span>
+      </Button>
     </div>
   );
 }
