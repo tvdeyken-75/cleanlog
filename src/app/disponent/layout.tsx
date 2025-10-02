00a27aae72f23c3cae6e3208cb926871edb582c2
Timothy Van der Eyken <unified.vandereyken@gmail.com>
2025-10-02 08:25:52 +0000

    change it again about minus 20px

--- src/app/disponent/layout.tsx
+++ src/app/disponent/layout.tsx
@@ -79,7 +79,7 @@ export default function DisponentLayout({
             </DialogContent>
           </Dialog>
         </div>
-        <div className="p-4 md:p-6 md:pl-20">
+        <div className="px-4 md:px-6 md:pl-20">
           {children}
         </div>
       </main>
